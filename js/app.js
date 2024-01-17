8bb4f4ff7f5ebec6ccd15c3aeba6a399219f5b68
Moi-ARM <moisesarcangel30@gmail.com>
2024-01-17 00:18:42 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function amor(){
+    Swal.fire({
+        title: "Te Amo Mi Vida",
+        text: "Eres la luz que ilumina mi camino.",
+        imageUrl: "fotos/Foto_Alerta.jpeg",
+        imageWidth: 300,
+        imageHeight: 210,
+        imageAlt: "Custom image",
+        confirmButtonText: "Continuar",
+        confirmButtonColor: "red"
+    });
+}
